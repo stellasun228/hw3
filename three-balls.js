6d75be5a9501398804365604fa9dee0d28dddb39
stellasun228 <60369544+stellasun228@users.noreply.github.com>
2020-02-12 15:43:04 -0800

    Update three-balls.js

--- three-balls.js
+++ three-balls.js
@@ -1,4 +1,4 @@
-var ballX1 = 25;
+var ballX1 = 15;
 var ballY1 = 100;
 var ballSpeed1 = 4;
 
@@ -6,10 +6,25 @@ var ballX2 = 250;
 var ballY2 = 25;
 var ballSpeed2 = 6;
 
-var ballX3 = 25;
-var ballY3 = 25;
-var ballZ3 = 0;
+var ballX3 = 30;
+var ballY3 = 30;
 var ballSpeed3 = 5;
+
+var ballX4 = 25;
+var ballY4 = 100;
+var ballSpeed4 = 4;
+
+var ballX5 = 250;
+var ballY5 = 25;
+var ballSpeed5 = 6;
+
+var ballX6 = 25;
+var ballY6 = 25;
+var ballSpeed6 = 5;
+
+var ballX7 = 25;
+var ballY7 = 25;
+var ballSpeed7 = 5;
 
 
 function setup() {
@@ -22,13 +37,13 @@ function draw() {
   
 // ball 1
   fill(220, 220, 240);
-  ellipse(ballX1, ballY1, 50);
+  ellipse(ballX1, ballY1, 30);
   ballX1 = ballX1 + ballSpeed1;
 
-  if (ballX1 == 25) {
+  if (ballX1 == 15) {
     ballSpeed1 = -ballSpeed1;
   }  
-  if (ballX1 > width-25) {
+  if (ballX1 > width-15) {
     ballSpeed1 = -ballSpeed1;
   }
 
@@ -46,14 +61,14 @@ function draw() {
   
 // ball 3
   fill(255, 204, 100);
-  ellipse(ballX3, ballY3, 50);
+  ellipse(ballX3, ballY3, 60);
   ballX3 = ballX3 + ballSpeed3;
   ballY3 = ballY3 + ballSpeed3;
 
-  if (ballX3 == 25 || ballY3 == 25) {
+  if (ballX3 == 30 || ballY3 == 30) {
     ballSpeed3 = -ballSpeed3;
   }  
-  if (ballX3 > width-25 || ballY3 > height-25) {
+  if (ballX3 > width-30 || ballY3 > height-30) {
     ballSpeed3 = -ballSpeed3;
   }
 }

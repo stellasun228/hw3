cc0275fc0fc8b4ee96154f1356cafc96999999ef
stellasun228 <60369544+stellasun228@users.noreply.github.com>
2020-02-13 13:34:03 -0800

    Create windy-lawnmower.js

---
+++ windy-lawnmower.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(400, 200);
+  colorMode(HSB);
+}
+
+var x = 0;
+var h = 10;
+
+function draw() {
+  stroke(random(60, 70), 100, 90);
+  //grass blades all slant to right
+  line(x, height-10, x+random(0, 25), height-10-random(h));
+  noStroke();
+
+  x = x + 10;
+
+  if (x > width) {
+    x = random(10);
+    h = h + 3;
+  }
+
+  // lawnmower cuts grass when grass reaches certain height
+  if (h > 60) {
+    fill(255);
+    rect(0, 0, width, height-15);
+    h = 10;
+  }
+
+  fill(40, 100, 60);
+  rect(0, height-10, width, 10);
+}
